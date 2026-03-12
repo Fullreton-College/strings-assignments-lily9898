8eaf19c140b3eb68b4c03086538c736535cd6b51
Kim, Lily <choiheewoo@gmail.com>
2026-03-12 14:35:28 -0700

    Code changes complete

--- String_Parser.cpp
+++ String_Parser.cpp
@@ -11,3 +11,29 @@ int main() {
 
    return 0;
 }
+
+void parseCSV(const string& csvFile) 
+{
+    ifstream inputFile(csvFile);
+    string studentRow;
+
+    while (getline(inputFile, studentRow)) 
+    {
+        stringstream input(studentRow);
+        string word;
+
+        while (input >> word)
+        {
+            for (int i = 0; i < word.length(); i++)
+            {
+                cout << word[i];
+
+                if (word[i] == ':')
+                    cout << " ";
+            }
+            cout << endl;
+        }
+
+        cout << endl;
+    }
+}
